8a74056d661e13dec4c14c92ad98112b5da7302d
Joe Cillo <JCillo507@yahoo.com>
2020-03-01 17:02:32 -0500

    info getting data

--- countries/src/components/CountryInfo/index.jsx
+++ countries/src/components/CountryInfo/index.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const CountryInfo = (props)=>{
   console.log(props)
+  const { data } = props.location.info
   return(
-    <div>working</div>
+    <div>
+      <h1>{data.name}</h1>
+  <p>{data.nativeName}</p>
+  <img src={data.flag}/>
+  <p>{data.population}</p>
+  <p>{data.region}</p>
+  <p>{data.subregion}</p>
+  <p>{data.capital}</p>
+  <p>{data.topLevelDomain}</p>
+  {/* <p>{data.currencies}</p> */}
+  {/* <p>{data.languages}</p> */}
+  {/* <p>{data.borderCountries}</p> */}
+    </div>
   )
 }
 
--- countries/src/components/ListOfCountries/index.jsx
+++ countries/src/components/ListOfCountries/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import CountryCard from "../Country/";
+import CountryCard from "../CountryCard";
 
 
 import { allCountryData } from "../services/ApiCall";
