01f0e892cc4c5243c0397dc3f9420ada289bd7f5
Joe Cillo <JCillo507@yahoo.com>
2020-04-01 16:22:23 -0400

    page stlye done

--- countries/src/components/CountryInfo/index.jsx
+++ countries/src/components/CountryInfo/index.jsx
@@ -51,8 +51,11 @@ class CountryInfo extends React.Component {
       .filter(e => borders.includes(e.code))
       .map(el => (
         <span key={el.country}>
-          <button className='border-button'>
-          <Link className='border'to={{ pathname: `/${el.country}` }}>{el.country}</Link></button>
+          <button className="border-button">
+            <Link className="border" to={{ pathname: `/${el.country}` }}>
+              {el.country}
+            </Link>
+          </button>
         </span>
       ));
     console.log(this.props);
@@ -64,32 +67,38 @@ class CountryInfo extends React.Component {
     ));
     const showInfo = data.map(data => (
       <div key={data.name} className="info-wrapper">
-        <img className='country-info-flag'src={data.flag} alt={data.name} />
+        <img className="country-info-flag" src={data.flag} alt={data.name} />
         <div className="info-ctr">
-          <h1 className='country-info-name'>{data.name}</h1>
-          <div className='data-wrapper'>
-          <div className='data-ctr'>
-          <InfoLine title="Native Name: " value={data.nativeName} />
-          <InfoLine title="Population: " value={data.population} />
-          <InfoLine title="Region: " value={data.region} />
-          <InfoLine title="Subregion: " value={data.subregion} />
-          <InfoLine title="Capital: " value={data.capital} /></div>
-          <div className='data-ctr'>
-          <InfoLine title="Top Level Domain: " value={data.topLevelDomain} />
-          <InfoLine title="Demonym: " value={data.demonym} />
-          <InfoLine title="Currency: " value={data.currencies[0].name} />
-          <div className='country-lang'>
-            <strong>Languages: </strong> {langArr}
+          <h1 className="country-info-name">{data.name}</h1>
+          <div className="data-wrapper">
+            <div className="data-ctr">
+              <InfoLine title="Native Name: " value={data.nativeName} />
+              <InfoLine title="Population: " value={data.population} />
+              <InfoLine title="Region: " value={data.region} />
+              <InfoLine title="Subregion: " value={data.subregion} />
+              <InfoLine title="Capital: " value={data.capital} />
+            </div>
+            <div className="data-ctr">
+              <InfoLine
+                title="Top Level Domain: "
+                value={data.topLevelDomain}
+              />
+              <InfoLine title="Demonym: " value={data.demonym} />
+              <InfoLine title="Currency: " value={data.currencies[0].name} />
+              <InfoLine title="Languages: " value={langArr} />
+            </div>
           </div>
-          </div></div>
-          <div>
-            <strong>Border Countries :</strong> {borderList}
+          <div className="border-ctr">
+            <strong className="border-title">Border Countries:</strong>{" "}
+            <span>
+              {borderList.length > 0 ? borderList : "No Border Countries"}
+            </span>
           </div>
         </div>
       </div>
     ));
     return (
-      <div className='country-info-ctr'>
+      <div className="country-info-ctr">
         <Link to={{ pathname: "/" }}>
           <button>Home</button>
         </Link>
